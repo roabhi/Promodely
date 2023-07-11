7aa84e8b3840728f59c5cdf7588251c1f89f0dab
roabhi <teskostudio@gmail.com>
2023-07-11 22:32:24 +0200

    fixed missing props in OverlayProduct for testing

--- src/components/OverlayProduct.test.tsx
+++ src/components/OverlayProduct.test.tsx
@@ -1,54 +1,56 @@
-import { render, screen } from "@testing-library/react";
-import Main from "./Main";
-import { Product } from "../types/Promofarma";
-import OverlayProduct from "./OverlayProduct";
+import { render, screen } from '@testing-library/react'
+import Main from './Main'
+import { Product } from '../types/Promofarma'
+import OverlayProduct from './OverlayProduct'
 
 const mockProducts: Product[] = [
   {
-    product_id: "1",
-    name: "Product 1",
+    product_id: '1',
+    name: 'Product 1',
     manufacturer: {
-      manufacturer_name: "Manufacturer 1",
+      manufacturer_name: 'Manufacturer 1',
     },
     main_category: {
-      category_name: "Category 1",
-      category_id: "",
+      category_name: 'Category 1',
+      category_id: '',
     },
     has_stock: true,
     recommended_prices: [
       {
         amount: 10,
-        currency: "€",
+        currency: '€',
       },
     ],
-    updated_at: "2023-07-11",
+    updated_at: '2023-07-11',
   },
-];
-describe("Given the nmain component", () => {
-  describe("when it is rendered", () => {
-    test("then it should render the main component", () => {
-      render(<Main products={mockProducts} loading={false} />);
+]
+describe('Given the nmain component', () => {
+  describe('when it is rendered', () => {
+    test('then it should render the main component', () => {
+      render(<Main products={mockProducts} loading={false} />)
 
-      expect(screen.getByText("Product 1")).toBeInTheDocument();
-    });
-  });
-});
+      expect(screen.getByText('Product 1')).toBeInTheDocument()
+    })
+  })
+})
 
-describe("Given the OverlayProduct component", () => {
-  describe("when it is rendered", () => {
-    test("then it should render the OverlayProduct component", () => {
+describe('Given the OverlayProduct component', () => {
+  describe('when it is rendered', () => {
+    test('then it should render the OverlayProduct component', () => {
       render(
         <OverlayProduct
-          name={""}
-          manufacturer={""}
-          category={""}
-          stock={""}
-          price={""}
+          name={''}
+          manufacturer={''}
+          category={''}
+          stock={''}
+          price={''}
+          id={''}
+          faved={''}
           {...OverlayProduct}
         />
-      );
+      )
 
       //expect(screen.getByText("Product 1")).toBeInTheDocument();
-    });
-  });
-});
+    })
+  })
+})
